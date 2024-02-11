82a040f4104b972d9d3b30c01602dccdfc06224b
RifqyYR <yudhiestrarifqy@gmail.com>
2024-02-11 22:56:07 +0800

    feat: setup request to API using retrofit

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,3 @@
+const String apiKey = "0f6d23c9f22848dfa747c8ed8f1c67cf";
+const String newsAPIBaseUrl = "https://newsapi.org/v2";
+const String country = "id";
---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,19 @@
+
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/constants.dart';
+import 'package:news_app/features/daily_news/data/models/article.dart';
+import 'package:retrofit/dio.dart';
+import 'package:retrofit/http.dart';
+
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseUrl)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET("/top-headlines")
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query("country") String? country,
+    @Query("apiKey") String? apiKey,
+  });
+}
